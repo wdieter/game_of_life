fd51dd51c21302046c9bee4d9391036af04b10e3
wdieter <wdieterich@gmail.com>
2020-11-29 13:12:06 +0100

    add click module for cli

--- life_driver.py
+++ life_driver.py
@@ -1,20 +1,25 @@
 import os
 import random
 from model import Grid
-x = 100
-y = 50
-
-cell_count = 500
-
-live_cells = [[random.randint(0,x), random.randint(0,y)] for _ in range(cell_count)]
-
-grid = Grid(x+1, y+1)
-grid.set_alive(live_cells)
-
-while True:
-    tmp = os.system('clear')
-    grid.play()
+import time
+import click
 
 
+@click.command()
+@click.option('--width', default=10, help='width of grid')
+@click.option('--height', default=10, help='height of grid')
+@click.option('--cells', default=20, help='number of cells to set alive at random locations in the grid')
+def main(width, height, cells):
+    live_cells = [[random.randint(0, width - 1), random.randint(0, height - 1)] for _ in range(cells)]
+
+    grid = Grid(width, height)
+    grid.set_alive(live_cells)
+
+    while not grid.stale:
+        tmp = os.system('clear')
+        grid.play()
+        time.sleep(0.5)
 
 
+if __name__ == '__main__':
+    main()
